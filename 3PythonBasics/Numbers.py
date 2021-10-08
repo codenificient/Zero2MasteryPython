9a30bbe5c8e920ee3ddd285d0ba6b2e95196554b
CodenificienT <61930093+codenificient@users.noreply.github.com>
2021-10-08 19:52:16 -0400

    new lectures

---
+++ 3PythonBasics/Numbers.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    # integer
+    print(3*78)
+    print(type(3*78))
+
+    # floats
+    print(3.2 * 32)
+    print(type(3.2*32))
---
+++ 3PythonBasics/bincomplex.py
@@ -0,0 +1,4 @@
+# bin and complex types
+print(bin(5)) # 0b101
+
+print(int('0b101', 2))
